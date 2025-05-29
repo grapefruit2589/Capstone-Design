54877a973816c0e08af70a74039658d9b9cac629
alalal4321 <alalal4321@naver.com>
2025-05-30 07:15:11 +0900

    Add splash screen + icon

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,95 +1,19 @@
 import 'package:flutter/material.dart';
+import 'SplashScreen.dart';
+import 'mainLoginScreen.dart';
 
-void main() => runApp(LoginApp());
+void main() => runApp(SilverOnApp());
 
-class LoginApp extends StatelessWidget {
+class SilverOnApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
-    );
-  }
-}
-
-class LoginScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '요양 보호사와\n노인 사용자를\n위한 플랫폼',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w500,
-                    height: 1.4,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 24),
-
-                // 중앙 아이콘 대체용
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.grey[300],
-                  child: Icon(Icons.person, size: 50, color: Colors.grey[700]),
-                ),
-
-                SizedBox(height: 24),
-                Text(
-                  '로그인',
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 32),
-
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Text('🧓'),
-                    label: Text('노인 사용자로 로그인'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue[700],
-                      foregroundColor: Colors.white,
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                      textStyle: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 12),
-
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Text('👩‍⚕️'),
-                    label: Text('요양 보호사로 로그인'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue[200],
-                      foregroundColor: Colors.black,
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                      textStyle: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 24),
-
-                Text(
-                  '사용자 유형을 선택해주세요!',
-                  style: TextStyle(fontSize: 14, color: Colors.grey[600]),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      title: '실버ON',
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(), // 첫 시작 화면
+      routes: {
+        '/login': (context) => LoginScreen(), // 로그인 화면 연결
+      },
     );
   }
 }
--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:capstone_design_project/main.dart';
+import 'package:cdp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
